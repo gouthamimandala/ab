2c7102aa72d2b8b6ac465290fa90f1aec77ba043
gouthamimandala <gouthamimandala9@gmail.com>
2022-04-25 16:25:53 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,18 @@
+from http.client import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def app1_rcb(request):
+    return HttpResponse('this is batch name')  
+
+
+
+
+
+
+
+
+
+
